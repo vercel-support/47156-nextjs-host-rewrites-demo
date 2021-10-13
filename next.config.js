dd36664855caa88207961995a846152c7c72f672
William Li <email@williamli.dev>
2021-10-13 18:20:00 +0800

    test rewrite in next.config.js, might end up with infinit loop

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'wood.47156-nextjs-host-rewrites-demo.vercel-support.app',
+          },
+        ],
+        destination: '/wood/:path*',
+      },
+    ]
+  }
 }
