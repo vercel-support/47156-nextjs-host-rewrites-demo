af7b1d09ceebdc6de301015bb5f099abb9187183
William Li <email@williamli.dev>
2021-10-25 23:21:39 +0800

    handling rewrite using beforeFiles

--- next.config.js
+++ next.config.js
@@ -1,28 +1,29 @@
 module.exports = {
   reactStrictMode: true,
-  trailingSlash: true,
   async rewrites() {
-    return [
-      {
-        source: '/:path((?!wood/).*)',
-        has: [
-          {
-            type: 'host',
-            value: 'wood.47156-nextjs-host-rewrites-demo.vercel-support.app',
-          },
-        ],
-        destination: '/wood/:path*',
-      },
-      {
-        source: '/',
-        // has: [
-        //   {
-        //     type: 'host',
-        //     value: 'wood.47156-nextjs-host-rewrites-demo.vercel-support.app',
-        //   },
-        // ],
-        destination: '/wood/',
-      },
-    ]
+    return {
+      beforeFiles: [
+        {
+          source: '/:path((?!wood/).*)',
+          has: [
+            {
+              type: 'host',
+              value: 'wood.47156-nextjs-host-rewrites-demo.vercel-support.app',
+            },
+          ],
+          destination: '/wood/:path*',
+        },
+        {
+          source: '/',
+          has: [
+            {
+              type: 'host',
+              value: 'wood.47156-nextjs-host-rewrites-demo.vercel-support.app',
+            },
+          ],
+          destination: '/wood/',
+        },
+      ]
+    }
   }
 }
