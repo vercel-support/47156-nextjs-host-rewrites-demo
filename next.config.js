99b4d857e3f6d94b991db5991c9fed43dc298fd3
William Li <email@williamli.dev>
2021-10-28 14:29:53 +0800

    try to handle all rewrites using 1 rule

--- next.config.js
+++ next.config.js
@@ -12,17 +12,7 @@ module.exports = {
             },
           ],
           destination: '/wood/:path*',
-        },
-        {
-          source: '/',
-          has: [
-            {
-              type: 'host',
-              value: 'wood.47156-nextjs-host-rewrites-demo.vercel-support.app',
-            },
-          ],
-          destination: '/wood/',
-        },
+        }
       ]
     }
   }
